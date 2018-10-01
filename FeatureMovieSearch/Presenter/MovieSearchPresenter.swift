7c56ca0370401e2e90356eb885eb32b8625c530d
Aldo Castro <aldo.castro21@gmail.com>
2018-10-01 09:32:58 +0200

    implementing presenter responsabilities

--- FeatureMovieSearch/Presenter/MovieSearchPresenter.swift
+++ FeatureMovieSearch/Presenter/MovieSearchPresenter.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import RxSwift
 
 public class MovieSearchPresenterFactory {
-    static func create() -> MovieSearchPresenter {
-        return MovieSearchPresenter()
+    static func create(view: MovieSearchView, searchMovieByTitleUseCase: SearchMovieByTitleUseCase) -> MovieSearchPresenter {
+        return MovieSearchPresenter(view: view, searchMovieByTitleUseCase: searchMovieByTitleUseCase)
     }
 }
 
 public class MovieSearchPresenter: NSObject {
     
+    private let view: MovieSearchView
+    private let searchMovieByTitleUseCase: SearchMovieByTitleUseCase
+    private let disposables: CompositeDisposable
+    
+    public init(view: MovieSearchView, searchMovieByTitleUseCase: SearchMovieByTitleUseCase) {
+        self.view = view
+        self.searchMovieByTitleUseCase = searchMovieByTitleUseCase
+        self.disposables = CompositeDisposable()
+        super.init()
+    }
+    
     public func onSearchTextChanged(_ text: String) {
-//        searchMovieByTitleUseCase.execute(text)
-        // TODO: rxify
+        let useCase = searchMovieByTitleUseCase
+            .execute(params: SearchMovieByTitleUseCase.Params(searchText: text))
+            .subscribe(
+                onSuccess: { searchResult in
+                    self.view.displayMovies(searchResult)
+                },
+                onError: { error in
+                    self.view.displayErrorView()
+                }
+        )
+        _ = disposables.insert(useCase)
+    }
+    
+    public func tearDown() {
+        disposables.dispose()
     }
 }
