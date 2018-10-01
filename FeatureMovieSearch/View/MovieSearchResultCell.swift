7b5ba1256703a47e85966e3bea7cc0f830c402f7
Aldo Castro <aldo.castro21@gmail.com>
2018-10-01 09:34:00 +0200

    implementing view responsabilities

---
+++ FeatureMovieSearch/View/MovieSearchResultCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieSearchResultCell.swift
+//  FeatureMovieSearch
+//
+//  Created by Aldo Castro on 30.09.18.
+//  Copyright © 2018 Castro, Aldo. All rights reserved.
+//
+
+import Foundation
+
+public class MovieSearchResultCell: UITableViewCell {
+    static let kCellIdentifier = "Cell"
+    @IBOutlet weak var title: UILabel!
+}
--- FeatureMovieSearch/View/MovieSearchViewController.swift
+++ FeatureMovieSearch/View/MovieSearchViewController.swift
@@ -10,25 +10,30 @@ import UIKit
 
 public class FeatureMovieSearchViewControllerFactory {
     
-    public static func create() -> MovieSearchViewController {
-        let presenter = MovieSearchPresenterFactory.create()
-        return MovieSearchViewController(presenter: presenter)
+    public static func create(searchMovieByTitleUseCase: SearchMovieByTitleUseCase) -> MovieSearchViewController {
+        return MovieSearchViewController(searchMovieByTitleUseCase: searchMovieByTitleUseCase)
     }
+}
+
+public protocol MovieSearchView {
+    func displayMovies(_ results: [SearchResult])
+    func displayErrorView()
 }
 
 public class MovieSearchViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    fileprivate var results: [SearchResult] = []
     fileprivate var presenter: MovieSearchPresenter!
     
     enum Constants: String {
         case nibName = "MovieSearchView"
     }
     
-    public required convenience init(presenter: MovieSearchPresenter) {
+    public required convenience init(searchMovieByTitleUseCase: SearchMovieByTitleUseCase) {
         self.init()
-        self.presenter = presenter
+        self.presenter = MovieSearchPresenter(view: self, searchMovieByTitleUseCase: searchMovieByTitleUseCase)
     }
 
     private init() {
@@ -40,10 +45,40 @@ public class MovieSearchViewController: UIViewController {
         return nil
     }
     
+    deinit {
+        presenter.tearDown()
+    }
+    
+    public override func viewDidLoad() {
+        tableView.register(MovieSearchResultCell.self, forCellReuseIdentifier: MovieSearchResultCell.kCellIdentifier)
+        tableView.dataSource = self
+    }
 }
 
 extension MovieSearchViewController : UISearchBarDelegate {
     public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         presenter.onSearchTextChanged(searchText)
+    }
+}
+
+extension MovieSearchViewController: MovieSearchView {
+    public func displayMovies(_ results: [SearchResult]) {
+        self.results = results
+        self.tableView.reloadData()
+    }
+    public func displayErrorView() {
+        //todo: display error view
+    }
+}
+
+extension MovieSearchViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MovieSearchResultCell.kCellIdentifier, for: indexPath)
+        cell.textLabel?.text = results[indexPath.row].title
+        return cell
     }
 }
